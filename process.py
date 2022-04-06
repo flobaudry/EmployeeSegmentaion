b1835eeb09093f168e09547735911487f18c92cc
flobaudry <33914132+flobaudry@users.noreply.github.com>
2022-04-06 02:20:42 +0200

    first implementation of scatter plot 3D

--- process.py
+++ process.py
@@ -1,5 +1,6 @@
 import base64
 import io
+from sklearn.cluster import KMeans
 
 import pandas as pd
 
@@ -8,3 +9,6 @@ def parse_csv(contents, filename):
     content_type, content_string = contents.split(",")
     decoded = base64.b64decode(content_string)
     df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+    df.columns = df.columns.str.replace(' ', '')
+    df["cat"] = 1
+    return df
