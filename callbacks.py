766d668e04c80a42a2f07f5456efd35245056ebd
flobaudry <33914132+flobaudry@users.noreply.github.com>
2022-04-07 23:28:18 +0200

    adding pie chart

--- callbacks.py
+++ callbacks.py
@@ -8,32 +8,61 @@ from dash.exceptions import PreventUpdate
 import dash_bootstrap_components as dbc
 from process import parse_csv
 
+df = None
 
-@app.callback(Output("graph", "children"),
+
+@app.callback(Output("3Dgraph", "children"),
+              Output("show_filename", "children"),
+              Output("pie_charts", "children"),
               Input("process-button", "n_clicks"),
               State("upload-employees", "contents"),
               State("upload-employees", "filename"))
 def process_data(n_clicks, contents, filename):
-    cluster_number = 3
-    res = html.Div(children=[])
+    res_3d = res_pie = html.Div(children=[])
     if contents is not None:
-        df, kmeans = parse_csv(contents, filename, cluster_number)
-        load_figure_template("darkly")
-        figure = px.scatter_3d(df, x="distance_in_m", y="time_in_s", z="CO2_in_g", color="transport_mean",
-                               template="darkly")
+        res_3d, res_pie = generate_graphs(contents, filename)
+    return res_3d, f"{filename} is currently used", res_pie
 
-        figure.add_trace(go.Scatter3d(x=kmeans.cluster_centers_[:, 0], y=kmeans.cluster_centers_[:, 1],
-                                      z=kmeans.cluster_centers_[:, 2], mode="markers", name="centroids",
-                                      marker=go.scatter3d.Marker(opacity=0.7, size=16, color=['blue', 'red', 'green'])))
-        if 'category' in df.columns:
-            fig = px.scatter_3d(df, x="distance_in_m", y="time_in_s", z="CO2_in_g", color="category", template="darkly")
-            res.children.append(
-                dbc.Row([dbc.Col([html.H2("Computed segmentation"), dcc.Graph(figure=figure)], width=6),
-                         dbc.Col([html.H2("Real segmentation"), dcc.Graph(figure=fig)], width=6)]))
-        else:
-            res.children.append(dbc.Row(dbc.Col(dcc.Graph(figure=figure))))
+
+def generate_pie_dropdown():
+    res = dbc.Row(children=[])
+    columns = ["CO2_in_g", "distance_in_m", "time_in_s"]
+    dropdown = dcc.Dropdown(columns, "CO2_in_g", id="pie-dropdown", searchable=False)
+    res.children.append(dbc.Col([dbc.Row(dropdown), dbc.Row(dcc.Graph(id="pie_chart_graph"))]))
     return res
 
+
+@app.callback(Output("pie_chart_graph", "figure"),
+              Input("pie-dropdown", "value"))
+def generate_pie_graphs(value):
+    return px.pie(df, values=value, names="transport_mean")
+
+
+def generate_graphs(contents, filename):
+    global df
+    cluster_number = 3
+    df, kmeans = parse_csv(contents, filename, cluster_number)
+    load_figure_template("darkly")
+
+    return generate_3d_graphs(kmeans), generate_pie_dropdown()
+
+
+def generate_3d_graphs(kmeans):
+    res = html.Div(children=[])
+    figure = px.scatter_3d(df, x="distance_in_m", y="time_in_s", z="CO2_in_g", color="transport_mean",
+                           template="darkly")
+
+    figure.add_trace(go.Scatter3d(x=kmeans.cluster_centers_[:, 0], y=kmeans.cluster_centers_[:, 1],
+                                  z=kmeans.cluster_centers_[:, 2], mode="markers", name="centroids",
+                                  marker=go.scatter3d.Marker(opacity=0.7, size=16, color=['blue', 'red', 'green'])))
+    if 'category' in df.columns:
+        fig = px.scatter_3d(df, x="distance_in_m", y="time_in_s", z="CO2_in_g", color="category", template="darkly")
+        res.children.append(
+            dbc.Row([dbc.Col([html.H2("Computed segmentation"), dcc.Graph(figure=figure)], width=6),
+                     dbc.Col([html.H2("Real segmentation"), dcc.Graph(figure=fig)], width=6)]))
+    else:
+        res.children.append(dbc.Row(dbc.Col(dcc.Graph(figure=figure))))
+    return res
 
 @app.callback(Output("collapse_button", "is_open"),
               Input("process-button", "n_clicks"),
--- layout.py
+++ layout.py
@@ -2,38 +2,37 @@ from dash import dcc, html
 import dash_bootstrap_components as dbc
 
 upload_layer = html.Div(children=[dbc.Row(dbc.Col(dcc.Upload(
-        id="upload-employees",
-        children=html.Div([
-            "Drag and Drop or ",
-            html.A("Select Files", className="alert-link")
-        ]),
-        style={
-            'width': '100%',
-            'height': '120px',
-            'lineHeight': '60px',
-            'borderWidth': '1px',
-            'borderStyle': 'dashed',
-            'borderRadius': '5px',
-            'textAlign': 'center',
-            'margin': '10px'
-        },
-        multiple=False
-    ), width=8), justify="center"),
+    id="upload-employees",
+    children=html.Div([
+        "Drag and Drop or ",
+        html.A("Select Files", className="alert-link")
+    ]),
+    style={
+        'width': '100%',
+        'height': '120px',
+        'lineHeight': '60px',
+        'borderWidth': '1px',
+        'borderStyle': 'dashed',
+        'borderRadius': '5px',
+        'textAlign': 'center',
+        'margin': '10px'
+    },
+    multiple=False
+), width=8), justify="center"),
     dbc.Row(dbc.Col(dbc.Alert("No file loaded", id="alert", color="danger"),
                     className="text-center", width=3), justify="center"),
     dbc.Row(dbc.Col(dbc.Button("Launch process", id="process-button", color="info", n_clicks=0),
                     className="d-grid gap-2", width=2), justify="center")])
 
-start_layout = html.Div(children=[
-    dbc.Collapse(dbc.Row(dbc.Col(dbc.Button("New upload", id="open_upload"), id="button_place", width=12),
-                         className="m-3", justify="center"), id="collapse_button", is_open=False),
+start_layout = dbc.Container(children=[
+    dbc.Collapse([dbc.Row([dbc.Col([dbc.Button("New upload", id="open_upload", className="h-100")],
+                                   id="button_place", width=2)],  justify="left", className="my-3"),
+                  dbc.Row(dbc.Col(dbc.Alert(id="show_filename", color="success")), className="my-3")],
+                 id="collapse_button", is_open=False, className="m-3"),
     dbc.Collapse(upload_layer, id="upload_collapse", is_open=True),
-    dbc.Row(dbc.Col(id='graph', width=12), className="m-3", justify="center")
+    dbc.Row(dbc.Col(id='3Dgraph', width=12), className="m-3", justify="center"),
+    dbc.Row(id="pie_charts", className="m-3")
 ]
-)
-
-fig_layout = html.Div(
-    dcc.Graph(id="graph")
 )
 
 layout = html.Div(
